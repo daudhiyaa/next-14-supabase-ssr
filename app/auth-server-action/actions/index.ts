afe7c59e4efcd806f4ad15e504b1bfa96caa8158
daudhiyaa <daud.dhiya31@gmail.com>
2024-04-21 18:56:46 +0700

    feat: handle signup to supabase

--- app/auth-server-action/actions/index.ts
+++ app/auth-server-action/actions/index.ts
@@ -1,10 +1,21 @@
+'use server';
+
+import createSupabaseServerClient from '@/lib/supabase/server';
+
 export async function signUpWithEmailAndPassword(data: {
-	email: string;
-	password: string;
-	confirm: string;
-}) {}
+  email: string;
+  password: string;
+  confirm: string;
+}) {
+  const supabase = await createSupabaseServerClient();
+  const res = await supabase.auth.signUp({
+    email: data.email,
+    password: data.password
+  });
+  return JSON.stringify(res);
+}
 
 export async function signInWithEmailAndPassword(data: {
-	email: string;
-	password: string;
+  email: string;
+  password: string;
 }) {}
