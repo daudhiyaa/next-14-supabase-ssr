a60f46361e948db0cdbe15ab927e3f0ef96fc59f
daudhiyaa <daud.dhiya31@gmail.com>
2024-04-21 19:11:53 +0700

    feat: add page-protection

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,40 +1,46 @@
-import React from "react";
-import CreateForm from "./components/CreateForm";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
+import React from 'react';
+import CreateForm from './components/CreateForm';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import readUserSession from '@/lib/actions';
+import { redirect } from 'next/navigation';
 
-export default function Page() {
-	const todos = [
-		{
-			title: "Subscribe",
-			created_by: "091832901830",
-			id: "101981908",
-			completed: false,
-		},
-	];
+export default async function Page() {
+  const todos = [
+    {
+      title: 'Subscribe',
+      created_by: '091832901830',
+      id: '101981908',
+      completed: false
+    }
+  ];
 
-	return (
-		<div className="flex justify-center items-center h-screen">
-			<div className="w-96 space-y-5">
-				<CreateForm />
+  const { data } = await readUserSession();
+  if (!data.session) {
+    return redirect('/auth-server-action');
+  }
 
-				{todos?.map((todo, index) => {
-					return (
-						<div key={index} className="flex items-center gap-6">
-							<h1
-								className={cn({
-									"line-through": todo.completed,
-								})}
-							>
-								{todo.title}
-							</h1>
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div className='w-96 space-y-5'>
+        <CreateForm />
 
-							<Button>delete</Button>
-							<Button>Update</Button>
-						</div>
-					);
-				})}
-			</div>
-		</div>
-	);
+        {todos?.map((todo, index) => {
+          return (
+            <div key={index} className='flex items-center gap-6'>
+              <h1
+                className={cn({
+                  'line-through': todo.completed
+                })}>
+                {todo.title}
+              </h1>
+
+              <Button>delete</Button>
+              <Button>Update</Button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
